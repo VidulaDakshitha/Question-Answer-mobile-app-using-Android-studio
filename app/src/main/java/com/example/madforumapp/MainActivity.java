e01a4540cd7c86b09faa4cc719bd539f7f743789
viduladakshitha <viduladakshitha@gmail.com>
2019-08-07 15:44:51 +0530

    added sign in page

--- app/src/main/java/com/example/madforumapp/MainActivity.java
+++ app/src/main/java/com/example/madforumapp/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.madforumapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn=findViewById(R.id.welcome);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i=new Intent(MainActivity.this,act2.class);
+
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/madforumapp/act2.java
@@ -0,0 +1,14 @@
+package com.example.madforumapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class act2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act2);
+    }
+}
