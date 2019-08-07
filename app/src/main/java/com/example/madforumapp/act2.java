9943e3d1851590d24662ace1b09713e3734298ad
viduladakshitha <viduladakshitha@gmail.com>
2019-08-07 19:44:13 +0530

    added register page

--- app/src/main/java/com/example/madforumapp/act2.java
+++ app/src/main/java/com/example/madforumapp/act2.java
@@ -2,13 +2,34 @@ package com.example.madforumapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class act2 extends AppCompatActivity {
+
+    TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act2);
+
+        register=findViewById(R.id.signup);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i=new Intent(act2.this,signup.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/madforumapp/signup.java
@@ -0,0 +1,23 @@
+package com.example.madforumapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class signup extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+
+    }
+
+
+}
